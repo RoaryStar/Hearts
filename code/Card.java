16d4fee906bef25a66ea143c651ca3317325252a
RoaryStar <hanssun07@gmail.com>
2018-01-15 09:34:49 -0500

    work for today

--- code/Card.java
+++ code/Card.java
@@ -2,51 +2,51 @@ import java.awt.*;
 
 public class Card extends Sprite
 {
-    protected int suit;
-    protected int value;
-    protected boolean face_up;
-    protected boolean selected;
-    
-    public Card()
-    {
+	protected int suit;
+	protected int value;
+	protected boolean face_up;
+	protected boolean selected;
+	
+	public Card()
+	{
 	super(Globals.CARD_UP_IMG, 0, new Point(0,0), Globals.CARD_DIM);
-    }
-    public Card(int s, int v, int layer, Point loc)
-    {
-	super(Globals.CARD_UP_IMG, layer, loc, Globals.CARD_DIM);
+	}
+	public Card(int s, int v, int layer, Point loc)
+	{
+	super(Globals.CARD_UP_IMG, new Point(Globals.CARD_DIM.x * v, Globals.CARD_DIM.y * s),layer, loc, Globals.CARD_DIM);
 	suit = s;
 	value = v;
 	face_up = true;
 	selected = false;
-    }
-    
-    public boolean get_face()
-    {
+	}
+	
+	public boolean get_face()
+	{
 	return face_up;
-    }
-    public void set_face(boolean faceup)
-    {
+	}
+	public void set_face(boolean faceup)
+	{
 	face_up = faceup;
-    }
-    public void flip()
-    {
+	}
+	public void flip()
+	{
 	face_up = !face_up;
-    }
-    
-    public int get_suit()
-    {
+	}
+	
+	public int get_suit()
+	{
 	return suit;
-    }
-    public int get_value()
-    {
+	}
+	public int get_value()
+	{
 	return value;
-    }
-    public int point_value()
-    {
+	}
+	public int point_value()
+	{
 	if (suit == Globals.HEARTS)
-	    return 1;
+		return 1;
 	if (suit == Globals.SPADES && value == 11)
-	    return 13;
+		return 13;
 	return 0;
-    }
+	}
 }
--- code/Main.java
+++ code/Main.java
@@ -1,18 +1,96 @@
 import java.applet.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.imageio.*;
 
-public class Main extends Applet
+public class Main extends Applet implements MouseListener, MouseMotionListener
 {
 	Game game;
 	
+	Graphics buffer_g;
+	Image offscreen;
+	Card c;
+	
+	long te = System.currentTimeMillis();
+		
 	public void init ()
 	{
-		// Place the body of the initialization method here
+		offscreen = createImage(getSize().width, getSize().height);
+		buffer_g = offscreen.getGraphics();
+		
+		try 
+		{
+			Globals.CARD_UP_IMG = ImageIO.read(new File("cards_up.gif"));
+			Globals.CARD_DOWN_IMG = ImageIO.read(new File("cards_down.gif"));
+			
+			Globals.CARD_DIM = new Point(
+					Globals.CARD_UP_IMG.getWidth(this) / 13,
+					Globals.CARD_UP_IMG.getHeight(this) / 4);
+		} 
+		catch (IOException e)
+		{
+			System.out.println("asdf");
+		}
+		
+		addMouseListener (this);
+		addMouseMotionListener (this);
 	}
-	
-	
+		
 	public void paint (Graphics g)
 	{
-		// Place the body of the drawing method here
+		double elapsed = System.currentTimeMillis() - te;
+		te = System.currentTimeMillis();
+	
+		buffer_g.clearRect(0, 0, getSize().width, getSize().height);
+		
+		c = new Card(Globals.rand.nextInt(4), Globals.rand.nextInt(13), 0, new Point(50,50));
+		c.draw(buffer_g);
+		
+		g.drawImage(offscreen, 0, 0, this);
+		
+		repaint(200);
+	}
+	
+	public void update(Graphics g)
+	{
+		long l = System.currentTimeMillis() - te;
+		try
+		{
+			if (l < 200)
+				Thread.sleep(200-l);
+		}
+		catch (InterruptedException e)
+		{
+		}
+		paint(g);
+	}
+	
+	public void mouseClicked (MouseEvent e)
+	{
+	}
+
+	public void mouseEntered (MouseEvent e)
+	{
+	}
+
+	public void mouseExited (MouseEvent e)
+	{
+	}
+
+	public void mousePressed (MouseEvent e)
+	{
+	}
+
+	public void mouseReleased (MouseEvent e)
+	{
+	}
+
+	public void mouseDragged (MouseEvent e)
+	{
+	}
+
+	public void mouseMoved (MouseEvent e)
+	{
 	}
 }
